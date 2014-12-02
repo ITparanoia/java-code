9eb91cd37678071ce24251da9a38b2b43e908866
arthinking <351108013@qq.com>
2014-12-02 18:16:28 +0800

    update

--- src/me/arthinking/code4jvmnote/C7_4_RoleEnum.java
+++ src/me/arthinking/code4jvmnote/C7_4_RoleEnum.java
@@ -2,7 +2,8 @@ package me.arthinking.code4jvmnote;
 
 /**
  * 
- * 被动应用例子，不同类型的常量对类初始化的影响
+ * 被动引用例子，不同类型的常量对类初始化的影响
+ * javap -verbose C7_4_RoleEnum
  * @author  Jason Peng
  * @create date 2014年12月2日
  */
@@ -63,7 +64,7 @@ class TestArrayConstant{
          * ldc     #21;
          * 要执行ldc指令，JVM首先查找index所指定的常量池入口，在index指向的常量池入口，JVM将会查找CONSTANT_Integer_info，CONSTANT_Float_info和CONSTANT_String_info入口。如果还没有这些入口，JVM会解析它们。而对于上面的hahaJVM会找到CONSTANT_String_info入口，同时，将把指向被拘留String对象（由解析该入口的进程产生）的引用压入操作数栈。
          * 
-         * 可以发现，数组常量会聪所在的类加载元素，而字符串和整型的常量会直接在常量池中保存一份数据，不会除非常量所在类的初始化。而C7_4_RoleEnum对象的实例常量也会在所在的类加载元素。
+         * 可以发现，数组常量会从所在的类加载元素，而字符串和整型的常量会直接在常量池中保存一份数据，不会除非常量所在类的初始化。而C7_4_RoleEnum对象的实例常量也会在所在的类加载元素。
          * 
          */
     }

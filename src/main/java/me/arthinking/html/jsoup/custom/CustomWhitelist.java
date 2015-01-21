91169dd909564a7cf43b28e83382e41e0f8d55c8
arthinking <351108013@qq.com>
2015-01-21 18:26:18 +0800

    change code logic

--- src/main/java/me/arthinking/html/jsoup/custom/CustomWhitelist.java
+++ src/main/java/me/arthinking/html/jsoup/custom/CustomWhitelist.java
@@ -35,7 +35,6 @@ public class CustomWhitelist extends Whitelist{
                 "th", "abbr", "axis", "colspan", "rowspan", "scope",
                 "width")
         .addAttributes("ul", "type")
-
         .addProtocols("a", "href", "ftp", "http", "https", "mailto")
         .addProtocols("blockquote", "cite", "http", "https")
         .addProtocols("cite", "cite", "http", "https")
@@ -46,11 +45,32 @@ public class CustomWhitelist extends Whitelist{
     
     /**
      * for fiter xss css style
+     * Jsoup只是根据判断是否属性外的文本进行编码，无法对可能产生的XSS问题进行编码，
+     * 所以可以在这里直接过滤掉可能产生XSS问题的内容
+     * 
+     * html传入Jsoup构建成Document的时候，会把Decimal HTML character解码，所以处理明确的编码就可以了
+     * 
+     * 过滤规则：
+     *   - Jsoup先转换为解码后的HTML
+     *   - 过滤script  style LINK标签
+     *   - 处理href src style 属性（javascript alert vbscript livescript），先过滤掉无用的字符（空格，tab，换行，注释）
+     *       style样式表禁止expression属性，禁止链接，防止远程样式表或脚本注入
+     *   - 各种标签的事件属性
+     * 
+     * 
+     * 
+     * jsoup只对设置了协议的属性进行过滤，只能判断跳转的协议是否合法，协议后的字符串注入不理会，这里需要处理下
+     *     在 Cleaner.copySafeNodes()里面获取安全的HTML
+     * 
      */
     @Override
     protected boolean isSafeAttribute(String tagName, Element el, Attribute attr) {
-        if(attr.getKey().equals("style")){
-            String value = attr.getValue().replaceAll(" ", "").toLowerCase();
+        if(attr.getKey().equals("style") || attr.getKey().equals("href")){
+            String value = attr.getValue().replaceAll(" ", "")
+                                          .replaceAll("\\r", "")
+                                          .replaceAll("\\n", "")
+                                          .replaceAll("\\*", "")
+                                          .toLowerCase();
             if(value.contains("javascript") 
                     || value.contains("script")
                     || value.contains("alert")
@@ -61,4 +81,13 @@ public class CustomWhitelist extends Whitelist{
         }
         return super.isSafeAttribute(tagName, el, attr);
     }
+
+    @Override
+    protected boolean isSafeTag(String tag) {
+        if(!super.isSafeTag(tag)){
+            // 校验失败，返回校验结果
+        }
+        return super.isSafeTag(tag);
+    }
+    
 }
--- src/main/java/me/arthinking/html/jsoup/custom/FilterTest.java
+++ src/main/java/me/arthinking/html/jsoup/custom/FilterTest.java
@@ -6,25 +6,34 @@ public class FilterTest {
 
     public static void main(String[] args) {
         CustomWhitelist htmlFilter = new CustomWhitelist();
-        htmlFilter.addTags("html", "body", "header", "url", "style");
-        htmlFilter.addAttributes(":all", "style", "class", "id", "name");
+        htmlFilter.addTags("html", "body", "header", "url", "style", "text", "img");
+        htmlFilter.addAttributes(":all", "src", "style");
         htmlFilter.addAttributes("object", "width", "height","classid","codebase");    
         htmlFilter.addAttributes("param", "name", "value");
+        htmlFilter.addAttributes("img", "src", "onload");
         htmlFilter.addAttributes("embed", "src","quality","width","height","allowFullScreen","allowScriptAccess","flashvars","name","type","pluginspage");
 
+        /*
         String html = ""
-                + "<header><url>a</url></header>"
                 + "<body>"
                 + "jaksjl<blockquote style=\"asadfjalsdf\">sdf</blockquote>"
                 + "<script></script>"
                 + "ab"
-                + "<STYLE >s</style>"
+                + "<a href=\"http://www.google.com/<abc>\">test</a>"
+                + "<STYLE >s<abc></style>"
+                + "<text>sfss>sdf</text>"
                 + "<b style=\"jav&#x0A;ascript:alert('XSS')\">Embedded carriage</b>"
                 + "<div style=\"Javas   cript:abc\" id=\"123\">space</div>"
                 + "<div style=\"java\0script:alert(\"XSS\")\">Null breaks up JavaScript directive</div>"
+                + "<img src=\"http://www.google.com/,ssd\" />"
+                + "<img src=\"&#0000106&#0000097&#0000118&#0000097&#0000115#0000108&#0000101&#0000114&#0000116&#0000040&#0000039&#0000088&#0000083&#0000083&#0000039&#0000041\" />"
                 + "</body>";
         // System.out.println(Jsoup.clean(html, htmlFilter));
         html += "<div style=\"&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A&#x61&#x6C&#x65&#x72&#x74&#x28&#x27&#x58&#x53&#x53&#x27&#x29\"></div>";
+         */
+        String html = "exp/*<A STYLE='no\\xss:noxss(\"*//*\");xss:ex/*XSS*//*/*/pression(alert(\"XSS\"))'>";
+        html="<div style='background:url(\"/i/eg_bg_03.gif\")'>test</div>";
+        // html = "background-image:\0075\0072\006C\0028'\006a\0061\0076\0061\0073\0063\0072\0069\0070\0074\003a\0061\006c\0065\0072\0074\0028.1027\0058.1053\0053\0027\0029'\0029";
         System.out.println(Jsoup.clean(html, htmlFilter));
         
     }

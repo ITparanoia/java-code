d522888e18105a73067210dc36c0635f55d4dbbe
arthinking <351108013@qq.com>
2015-01-30 00:20:23 +0800

    add code

--- src/main/java/me/arthinking/app/datatransform/controller/UserWrap.java
+++ src/main/java/me/arthinking/app/datatransform/controller/UserWrap.java
@@ -1,39 +1,53 @@
 package me.arthinking.app.datatransform.controller;
 
-import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ser.FilterProvider;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.alibaba.fastjson.serializer.JSONSerializer;
+import com.alibaba.fastjson.serializer.PropertyFilter;
+import com.alibaba.fastjson.serializer.SerializeWriter;
 
 public class UserWrap {
 
-	public static String toJsonObject() throws JsonGenerationException, JsonMappingException, IOException{
+	public static String toJsonObject(){
 		
 		User user = new User();
         //create ObjectMapper instance
-        ObjectMapper objectMapper = new ObjectMapper();
+        // ObjectMapper objectMapper = new ObjectMapper();
         Set<String> properties = new HashSet<String>();
         properties.add("username");
-        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("myFilter",  
-                SimpleBeanPropertyFilter.serializeAllExcept("username", "id"));  
-        objectMapper.setFilters(filterProvider);
+        // FilterProvider filterProvider = new SimpleFilterProvider().addFilter("myFilter",  
+        //         SimpleBeanPropertyFilter.serializeAllExcept("username", "id"));  
+        // objectMapper.setFilters(filterProvider);
         // objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
         StringWriter stringEmp = new StringWriter();
         // objectMapper.writeValue(stringEmp, user);
-        System.out.println(objectMapper.writeValueAsString(user));
+        // System.out.println(objectMapper.writeValueAsString(user));
         // String userJson = Jacksons.me().filter("myFilter", "username").readAsString(user);
         // System.out.println(userJson);
+        // http://kkrgwbj.iteye.com/blog/2001031
+        // SimplePropertyPreFilter filter = new SimplePropertyPreFilter(User.class, "username");
+        PropertyFilter filter = new PropertyFilter() {  
+            //过滤不需要的字段  
+            public boolean apply(Object source, String name, Object value) {  
+                if("username".equals(name)||"id".equals(name)){  
+                    return false;  
+                }  
+                return true;  
+            }  
+        };
+        SerializeWriter sw = new SerializeWriter();  
+        JSONSerializer serializer = new JSONSerializer(sw);  
+        serializer.getPropertyFilters().add(filter);
+        serializer.write(user);  
+        System.out.println(sw.toString());  
+        // System.out.println(JSON.toJSONString(user, SerializerFeature.WriteMapNullValue));
+        // System.out.println(JSON.toJSONString(user, filter, SerializerFeature.WriteMapNullValue));
 		return "";
 	}
 	
-	public static void main(String[] args) throws JsonGenerationException, JsonMappingException, IOException{
+	public static void main(String[] args){
 		toJsonObject();
 	}
 	
